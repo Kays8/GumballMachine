162ca122138839cdbe7b6ffeaf390ebdb2f1e316
Kays8 <thirayut1379@gmail.com>
2024-08-29 16:31:15 +0700

    GumballMachine

---
+++ src/GumballMachineTestDrive.java
@@ -0,0 +1,22 @@
+
+public class GumballMachineTestDrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GumballMachine gumballMachine = new GumballMachine(5);
+		
+		
+		
+		gumballMachine.ejectQuarter();
+		gumballMachine.insertQuarter();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.turnCrank();
+		System.out.println(gumballMachine);
+		
+		
+		//System.out.println(gumballMachine);
+
+	}
+
+}
